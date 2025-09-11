1414c303f70c698a9b897381f03e2db7f9b02e78
veccyboi <ethanyunzeli@gmail.com>
2025-09-11 08:31:12 -0500

    attempt to restore compatibility with dye depot (still broken)

--- common/src/main/java/net/azurune/delicate_dyes/core/mixin/DyeColorMixin.java
+++ common/src/main/java/net/azurune/delicate_dyes/core/mixin/DyeColorMixin.java
@@ -11,6 +11,10 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.util.StringRepresentable;
+import net.azurune.delicate_dyes.DelicateDyes;
+
+import java.util.Arrays;
 
 @Mixin(DyeColor.class)
 public class DyeColorMixin {
@@ -29,7 +33,7 @@ public class DyeColorMixin {
         DyeColor[] addDye = new DyeColor[length + 8];
         System.arraycopy($VALUES, 0, addDye, 0, length);
 
-        addDye[length + 0] = delicateDyes$init("CORAL", getDyeCount + 1, getDyeCount + 1, "coral",
+        addDye[length + 0] = delicateDyes$init("DELICATEDYES_CORAL", getDyeCount + 1, getDyeCount + 1, "delicatedyes_coral",
                 0xf97474, MapColor.RAW_IRON, 0xff6565, 0xff6565);
 
         addDye[length + 1] = delicateDyes$init("CANARY", getDyeCount + 2, getDyeCount + 2, "canary",
@@ -50,9 +54,10 @@ public class DyeColorMixin {
         addDye[length + 6] = delicateDyes$init("SANGRIA", getDyeCount + 7, getDyeCount + 7, "sangria",
                 0x82125a, MapColor.TERRACOTTA_PURPLE, 0x821d5e, 0x821d5e);
 
-        addDye[length + 7] = delicateDyes$init("ROSE", getDyeCount + 8, getDyeCount + 8, "rose",
+        addDye[length + 7] = delicateDyes$init("DELICATEDYES_ROSE", getDyeCount + 8, getDyeCount + 8, "delicatedyes_rose",
                 0xc81b4f, MapColor.CRIMSON_HYPHAE, 0xbe2e59, 0xbe2e59);
 
         $VALUES = addDye;
+        DelicateDyes.LOGGER.info("all colors present after delicate dyes:" + Arrays.toString($VALUES));
     }
 }
